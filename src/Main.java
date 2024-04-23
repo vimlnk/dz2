d0d8d0825e15637651ed0ec05b555f53c733a5e1
Admin <melnukvikysa@gmail.com>
2024-04-23 22:03:33 +0300

    commit class cat

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,42 @@
 class Animals {
-    static int totalAn = 0;
+    protected int maxRun;
+    protected int maxSwim;
 
-    Animals(){
-        totalAn++;
+    public Animals(int maxRun, int maxSwim) {
+        this.maxRun = maxRun;
+        this.maxSwim = maxSwim;
     }
-    void run(int distance){
-        System.out.println("Тварина пробігла "+ distance +"м.");
+
+    void run(int distance) {
+        if (distance < maxRun) {
+            System.out.println("Тварина пробігла "+ distance);
+        } else {
+            System.out.println("Тварина не може пробігти "+ distance);
+
+        }
+
     }
-    void swim(int distance){
-        System.out.println("Тварина проплила "+ distance+ "м.");
+    void swim(int distance) {
+        if (distance < maxSwim) {
+            System.out.println("Тварина проплила " + distance);
+        } else if (distance > maxSwim) {
+            System.out.println("Тварина не вміє плавати");
+        }else {
+            System.out.println("Тварина не може проплисти "+ distance);
+        }
     }
 }
 class Cat extends Animals {
-
+ public Cat() {
+    super(201,0);
 }
-class Dog extends Animals{
-
 }
+ public class Main {
+     public static void main(String[] args) {
+         Cat cat = new Cat();
+         cat.run(200);
+         cat.run(300);
+         cat.swim(200);
+     }
+ }
+
